9c53738fe0935c7299bb0c44398cc0f0d43af597
tanguy <tanguy@getboxy.co>
2022-05-21 22:33:31 +0200

    init repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import React, { useEffect, useState } from 'react'
+
+
+const AppDiv = styled.div`
+`
+
+export const App = () => {
+	return (
+		<div>
+		</div>
+	)
+}
